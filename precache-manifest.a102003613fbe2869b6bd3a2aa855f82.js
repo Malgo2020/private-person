b2a1ed95b4ca3b0bbe12cbd4041431fcc9e34287
MalgoBlock <mmmzemla@gmail.com>
2020-09-11 08:45:34 +0200

    Updates

---
+++ precache-manifest.a102003613fbe2869b6bd3a2aa855f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d9ea69a9983279cd633545dbefd507",
+    "url": "/private-person/index.html"
+  },
+  {
+    "revision": "a1a83bc545874ff46a9b",
+    "url": "/private-person/static/css/main.7c856dbb.chunk.css"
+  },
+  {
+    "revision": "299056e88c19b8597602",
+    "url": "/private-person/static/js/2.2867f221.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/private-person/static/js/2.2867f221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a83bc545874ff46a9b",
+    "url": "/private-person/static/js/main.35bdaeac.chunk.js"
+  },
+  {
+    "revision": "606ab891ad7dc9185040",
+    "url": "/private-person/static/js/runtime-main.aa7f8d29.js"
+  }
+]);
